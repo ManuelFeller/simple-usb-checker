23fac109f590ae08227b25be85693b6e48b63c4d
Manuel Feller <manuel.feller@boehringer-ingelheim.com>
2022-01-14 15:17:45 +0100

    adding first electron host code pls demo of usb detection

---
+++ src/main.ts
@@ -0,0 +1,59 @@
+import usbDetect from 'usb-detection';
+import { app, BrowserWindow }from 'electron';
+
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 320,
+    height: 440
+  });
+
+  win.loadFile('./static/test.html');
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+	// needed to also work on MacOS as expected
+	app.on('window-all-closed', function () {
+		if (process.platform !== 'darwin') app.quit()
+	});
+	app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  });
+
+});
+
+/*
+usbDetect.startMonitoring();
+
+// Detect add/insert
+usbDetect.on('add', function(device) { console.log('add', device); });
+
+// Detect remove
+usbDetect.on('remove', function(device) { console.log('remove', device); });
+
+// Detect add or remove (change)
+usbDetect.on('change', function(device) { console.log('change', device); });
+
+// Get a list of USB devices on your system, optionally filtered by `vid` or `pid`
+usbDetect.find(function(err, devices) { console.log('find', devices, err); });
+//usbDetect.find(vid, function(err, devices) { console.log('find', devices, err); });
+//usbDetect.find(vid, pid, function(err, devices) { console.log('find', devices, err); });
+// Promise version of `find`:
+//usbDetect.find().then(function(devices) { console.log(devices); }).catch(function(err) { console.log(err); });
+
+// Allow the process to exit
+usbDetect.stopMonitoring()
+*/
+/*
+{
+	locationId: 85012480,
+	vendorId: 1193,
+	productId: 6144,
+	deviceName: 'TS8000 series',
+	manufacturer: 'Canon',
+	serialNumber: '12BAE2',
+	deviceAddress: 8
+}
+*/
