d266e86b65e46b8308f438d8b4dfdbe5da7cfafe
Manuel Feller <github@devsvr.ws>
2022-01-15 22:21:06 +0100

    adding jsdoc and singleton code

--- src/usb-detector.ts
+++ src/usb-detector.ts
@@ -1,11 +1,18 @@
 import usbDetect from 'usb-detection';
 
+/**
+ * Class that handles the USB monitoring; wrapping the usb-detect package
+ */
 export default class UsbDetector {
 
 	private currentDevices: usbDetect.Device[];
 	private initialized = false;
 	private onChangeHandler: Function | undefined;
+	static instance: UsbDetector;
 
+	/**
+	 * !!! Do not manually creat new instances ! Use UsbDetector.getInstance() !!!
+	 */
 	constructor() {
 		console.log('DETECTOR: UsbDetector class created');
 		this.currentDevices = []
@@ -13,6 +20,21 @@ export default class UsbDetector {
 		usbDetect.find(this.processInitialListing.bind(this));
 	}
 
+	/**
+	 * Get the singleton instance of the class
+	 * @returns The instance to use
+	 */
+	public static getInstance(): UsbDetector {
+		if (UsbDetector.instance === undefined) {
+			UsbDetector.instance = new UsbDetector();
+		}
+		return UsbDetector.instance;
+	} 
+
+	/**
+	 * Get the list of connected USB devices
+	 * @returns The list of the currently connected devices
+	 */
 	public getDeviceList(): usbDetect.Device[] {
 		if (!this.initialized) {
 			console.error('DETECTOR: Not fully initialized');
@@ -21,10 +43,20 @@ export default class UsbDetector {
 		return this.currentDevices;
 	}
 
+	/**
+	 * "Register" the callback for the case that there is a change in teh USB device list
+	 * @param handler The function that handles the change events
+	 */
 	public onUsbChange(handler: Function | undefined) {
 		this.onChangeHandler = handler;
 	}
 
+	/**
+	 * Internal code to process the initial list of devices
+	 * @param error In case of an error this gets filled with details
+	 * @param data In case of success this gets filled with the list of devices
+	 * @returns An array with usbDetect.Device objects
+	 */
 	private processInitialListing(error: any, data: usbDetect.Device[]) {
 		console.log('DETECTOR: Processing initial USB Device List');
 		if (error !== undefined) {
@@ -39,6 +71,10 @@ export default class UsbDetector {
 		usbDetect.on('remove', this.onDeviceRemove.bind(this))
 	}
 
+	/**
+	 * Internal code that handles the event of newly added devices
+	 * @param device The usbDetect.Device that was added
+	 */
 	private onDeviceAdd(device: usbDetect.Device) {
 		console.log('DETECTOR: Add detected');
 		console.log(device);
@@ -50,6 +86,10 @@ export default class UsbDetector {
 		this.currentDevices.push(device);
 	}
 
+		/**
+	 * Internal code that handles the event of removed devices
+	 * @param device The usbDetect.Device that was removed
+	 */
 	private onDeviceRemove(device: usbDetect.Device) {
 		console.log('DETECTOR: Remove detected');
 		console.log(device);
@@ -73,7 +113,17 @@ export default class UsbDetector {
 	}
 }
 
+/**
+ * Interface to describe the data sent out in case something
+ * in the list of connected USB devices changes
+ */
 export interface UsbChangeEvent {
+	/**
+	 * The event type
+	 */
 	type: 'add' | 'remove',
+	/**
+	 * The device details
+	 */
 	device: usbDetect.Device
 }
